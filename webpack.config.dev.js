3c8a6295c35ce8f20d293fde29e50d12aa097910
Mohammed Basheer <ssp.basheer@gmail.com>
2018-07-15 09:26:44 +0800

    Initial commit with basic setup

---
+++ webpack.config.dev.js
@@ -0,0 +1,29 @@
+import webpack from 'webpack';
+import path from 'path';
+
+export default {
+    mode: 'development',
+    devtool: 'inline-source-map',
+    entry: [
+        path.resolve(__dirname, 'src/index')
+    ],
+    target: 'web',
+    output: {
+        path: __dirname + '/dist',
+        publicPath: '/',
+        filename: 'bundle.js'
+    },
+    devServer: {
+        contentBase: path.resolve(__dirname, 'src')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: path.join(__dirname, 'node_modules'),
+                include: path.join(__dirname, 'src'),
+                use: 'babel-loader',
+            }
+        ]
+    }
+};
